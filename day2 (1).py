048ff7fdaa8b197693016aa38a8e6f7727f02f28
Priya Gupta <87631126+Priyagpt353@users.noreply.github.com>
2021-07-30 11:11:23 +0530

    Check whether a string is a pangram.

---
+++ day2 (1).py
@@ -0,0 +1,12 @@
+
+#Ques 2 - Check whether a string is a pangram.
+
+from string import ascii_lowercase as asc_lower
+def check(s):
+    return set(asc_lower) - set(s.lower()) == set([])
+strng=input("Enter string:")
+if(check(strng)==True):
+      print("The string is a pangram")
+else:
+      print("The string isn't a pangram")
+
